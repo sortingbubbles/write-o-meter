01ad42b550a779b11bd0157a44d45bd6147ad1a0
Annie Chatsatrian <sortingbubbles@outlook.com>
2020-09-17 00:34:53 +0200

    feat(app): setup initial react app

--- js/script.js
+++
@@ -1,4 +0,0 @@
-function getNumOfWords(){
-        var numOfWords = $.trim($('[name="textAnalysis"]').val()).split(' ').filter(function(v){return v!==''}).length;
-        document.getElementById("wordCount").innerText = numOfWords;
-}
