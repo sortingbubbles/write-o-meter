b3674f1c1e90c9046718b4b9d78c3b5dcc831f48
Ani Chatsatrian <sortingbubbles@outlook.com>
2020-09-17 00:44:09 +0200

    feat: switch to react (#1)
    * feat(app): setup initial react app
    
    * feat(App): add textarea in the page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form>
+          <h1>write-o-meter</h1>
+          <textarea></textarea> 
+        </form>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
