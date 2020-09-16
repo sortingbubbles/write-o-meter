5c7327af99fe7b2c0c08cfcf6800576a8834b2c1
Annie Chatsatrian <sortingbubbles@outlook.com>
2020-09-17 01:09:48 +0200

    Updates

---
+++ precache-manifest.239ce490b6ff59f46255731db9b614fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe69b08b1defb7e64e1767aecf50b42",
+    "url": "/write-o-meter/index.html"
+  },
+  {
+    "revision": "98f3dc467a44c1a2d285",
+    "url": "/write-o-meter/static/css/main.54d98e33.chunk.css"
+  },
+  {
+    "revision": "700eb42b316904084016",
+    "url": "/write-o-meter/static/js/2.3a02bd01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/write-o-meter/static/js/2.3a02bd01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f3dc467a44c1a2d285",
+    "url": "/write-o-meter/static/js/main.22b3d091.chunk.js"
+  },
+  {
+    "revision": "2d915d5cfaa20a927df0",
+    "url": "/write-o-meter/static/js/runtime-main.8398692c.js"
+  }
+]);
