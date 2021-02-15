9336eadaf2d5751731b75c575fa31a014966f375
Ani Chatsatrian <sortingbubbles@outlook.com>
2021-02-16 00:59:32 +0100

    fix: add content-type

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,13 @@
 export const API_HOST = 'https://vzjega.deta.dev'
 
 export function analyze(text: string) {
-  return fetch(
-    `${API_HOST}/analyse`, {
+  return fetch(`${API_HOST}/analyse`, {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({
-      text
+      text,
     }),
   })
 }
