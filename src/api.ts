2dfc418750f99ddc95114d44d08151d51a6a1f0d
Ani Chatsatrian <sortingbubbles@outlook.com>
2021-02-16 00:30:55 +0100

    feat: add api file

---
+++ src/api.ts
@@ -0,0 +1,9 @@
+export const API_HOST = 'https://vzjega.deta.dev'
+
+export function analyze(text: string) {
+  return fetch(
+    `${API_HOST}/analyse`, {
+    method: 'POST',
+    body: JSON.stringify(text),
+  })
+}
