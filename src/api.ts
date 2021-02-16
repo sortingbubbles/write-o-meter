f934c4269d9483aeba746c286b4d67231f2cc537
Ani Chatsatrian <sortingbubbles@outlook.com>
2021-02-16 01:03:11 +0100

    fix: typo

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 export const API_HOST = 'https://vzjega.deta.dev'
 
 export function analyze(text: string) {
-  return fetch(`${API_HOST}/analyse`, {
+  return fetch(`${API_HOST}/analyze`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
