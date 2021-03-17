de854b380a04a6b93f9b3909257e76a168f44752
Ani Chatsatrian <sortingbubbles@outlook.com>
2021-03-17 18:45:40 +0100

    feat: add endpoint for custom algorithms

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,21 @@
-export const API_HOST = 'https://vzjega.deta.dev'
+// export const API_HOST = 'https://vzjega.deta.dev'
+export const API_HOST = 'http://localhost:8000'
 
-export function analyze(text: string) {
-  return fetch(`${API_HOST}/analyze`, {
+export function analyzeWithSpacy(text: string) {
+  return fetch(`${API_HOST}/analyze-spacy`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      text,
+    }),
+  })
+}
+
+
+export function analyzeWithCustomAlgorithms(text: string) {
+  return fetch(`${API_HOST}/analyze-custom`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
