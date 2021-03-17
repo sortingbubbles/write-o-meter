79de1434fc2ffb7e8630569066f9b9a0a28b092d
Ani Chatsatrian <sortingbubbles@outlook.com>
2021-03-17 17:43:52 +0100

    feat: add method switching

--- src/WriteOMeterForm.js
+++ src/WriteOMeterForm.js
@@ -6,9 +6,11 @@ class WriteOMeterForm extends React.Component {
     super(props)
     this.state = {
       value: 'Γράψτε εδώ το κείμενο προς ανάλυση.',
-      results: null
+      method: 'spacy-lib',
+      results: null,
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleAnalysisMethodChange = this.handleAnalysisMethodChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.showResults = this.showResults.bind(this)
   }
@@ -17,17 +19,18 @@ class WriteOMeterForm extends React.Component {
     this.setState({ value: event.target.value })
   }
 
+  handleAnalysisMethodChange(event) {
+    this.setState({ method: event.target.value })
+  }
+
   showResults() {
-    if (this.state.results) return (
-      <div>
-        <p>
-          Προτάσεις: {this.state.results.sents.length}
-        </p>
-        <p>
-          Λέξεις: {this.state.results.words.length}
-        </p>
-      </div>
-    )
+    if (this.state.results)
+      return (
+        <div>
+          <p>Προτάσεις: {this.state.results.sents.length}</p>
+          <p>Λέξεις: {this.state.results.words.length}</p>
+        </div>
+      )
   }
 
   async handleSubmit(event) {
@@ -43,12 +46,42 @@ class WriteOMeterForm extends React.Component {
 
   render() {
     const hasResults = !!this.state.results
+    const isUsingSpacyLibrary = this.state.isUsingSpacyLib
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <textarea value={this.state.value} onChange={this.handleChange} />{' '}
-        </label>
-        <input type="submit" value="Aνάλυση" />
+      <form className="wom-form" onSubmit={this.handleSubmit}>
+        <section className="wom-form__controls">
+          <label className="wom-form__text-area">
+            <textarea value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <section className="wom-form__controls__method-options">
+            <h3>Ανάλυση με</h3>
+            <p>
+              <label>
+                τη βιβλιοθήκη SpaCy
+                <input
+                  type="radio"
+                  value="spacy-lib"
+                  checked={this.state.method === 'spacy-lib'}
+                  onChange={this.handleAnalysisMethodChange}
+                />
+                <span className="wom-form__controls__method-options__radio"></span>
+              </label>
+            </p>
+            <p>
+              <label>
+                τους αλγορίθμους γραμματικής
+                <input
+                  type="radio"
+                  value="grammatical-algorithms"
+                  checked={this.state.method === 'grammatical-algorithms'}
+                  onChange={this.handleAnalysisMethodChange}
+                />
+                <span className="wom-form__controls__method-options__radio"></span>
+              </label>
+            </p>
+          </section>
+        </section>
+        <input className="wom-form__input" type="submit" value="Aνάλυση" />
         {hasResults && this.showResults()}
       </form>
     )
